b7ebf01724fafbd80d8f566d557b956eb130fda8
victor Santos Uceta <victor.uceta@gmail.com>
2017-02-14 12:44:55 -0500

    adding write benchmark results

--- movies-graph-populate-edges-batch.js
+++ movies-graph-populate-edges-batch.js
@@ -11,12 +11,7 @@ const edges = require('./movie/edges.json');
 
 /* Instantiate connection */
 var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neoj4"));
- 
-/* Register a callback to know if driver creation was successful */
-driver.onCompleted = function() {
-  /* proceed with using the driver, it was successfully instantiated */
-  console.log('Driver instantiation success');
-};
+
 
 /* Register a callback to know if driver creation failed. */
 /* This could happen due to wrong credentials or database unavailability */
@@ -26,54 +21,50 @@ driver.onError = function(error) {
 
 /* Create a session to run Cypher statements in. */
 var session = driver.session();
+const batchSize = 300;
 
+let eQueue = Object.keys(edges);
+let total = eQueue.length, current = 0;
+console.log(total);
 
-trueno.connect((s)=> {
+/* Register a callback to know if driver creation was successful */
+driver.onCompleted = function() {
+  /* proceed with using the driver, it was successfully instantiated */
+  console.log('Driver instantiation success');
 
-  const batchSize = 300;
-
-  /* Create a new Graph */
-  let g = trueno.Graph();
-
-  /* Set label: very important */
-  g.setLabel(dbName);
-
-  let eQueue = Object.keys(edges);
-  let total = eQueue.length, current = 0;
-  console.log(total);
-
+  console.time("main");
   /* Insertion function */
   function insertEdge(arr) {
 
+    var query = [];
     /* Persist all vertices */
     arr.forEach((ekey)=> {
       let edgePair = edges[ekey];
-      let e = g.addEdge(edgePair.source, edgePair.destination);
-      e.setLabel(edgePair.label);
-      e.setId(current);
-      e.persist();
-      current++;
+      query.push("CREATE (a" +edgePair.source+")-[b"+ekey +":Edge]->(a"+edgePair.destination+")");
+      //query.push("CREATE (a" +edgePair.source+")-[b"+ekey +":Edge {source: '"+edgePair.source +"', destination: '"+edgePair.destination+"', label: '"+ edgePair.label+ "'}]->(a"+edgePair.destination+")");
     });
 
-    session.run(query, param)
+    //console.time("time");
+    session.run(query.join('\n'))
     .then(function(result){
-        console.log("Edges batch created.", current / total);
+        //console.timeEnd("time");
         /* Continue inserting */
         if (eQueue.length) {
           insertEdge(eQueue.splice(0, batchSize));
         } else {
-          console.timeEnd("time");
+          console.timeEnd("main");
           session.close();
           process.exit();
         }
     })
     .catch(function(error) {
+        console.timeEnd("time");
         console.log("Error: Edges batch creation failed.", error, current / total);
         /* Continue inserting */
         if (eQueue.length) {
           insertEdge(eQueue.splice(0, batchSize));
         } else {
-          console.timeEnd("time");
+          console.timeEnd("main");
           session.close();
           process.exit();
         }
@@ -82,3 +73,5 @@ trueno.connect((s)=> {
 
   /* Initiating vertex insertion */
   insertEdge(eQueue.splice(0, batchSize));
+
+};
